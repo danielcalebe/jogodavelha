14aeb743940c75d5b391bf30e1bb83eac6ce73ce
Daniel Calebe <160525339+danielcalebe@users.noreply.github.com>
2025-02-17 16:53:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ document.querySelector('.theme-toggle').addEventListener('click', () => {
         }, [100])
 
         player = player === "X" ? "O" : "X";
-        playerFront = player === "X" ?  "Player 1" : "Player 2"
+        playerFront = player == "X" ?  "Player 1" : "Player 2"
 
         currentPlayer.innerHTML = `VEZ DE: ${playerFront}`;
 
